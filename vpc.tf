f07cb5bd3ad8b09cfb204888b6e3368a672d65e0
balu <65383476+baluk2249@users.noreply.github.com>
2025-04-21 21:32:14 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_network" {
   name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
   mtu                     = 1460
 }
