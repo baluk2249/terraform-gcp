42eed364c8ec10eca4191b6a932325262cb64c8c
balu <65383476+baluk2249@users.noreply.github.com>
2025-04-21 22:58:21 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,34 +1,14 @@
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
-  auto_create_subnetworks = false
-  mtu                     = 1460
-}
-resource "google_compute_subnetwork" "default" {
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
-  network       = google_compute_network.vpc_network.id
-}
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc_network.id
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
+
+# VPC
+resource "google_compute_network" "vpc" {
+  name                    = "vpc-gke"
+  auto_create_subnetworks = "false"
 }
 
-resource "google_compute_firewall" "flask" {
-  name    = "flask-app-firewall"
-  network = google_compute_network.vpc_network.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5000"]
-  }
-  source_ranges = ["0.0.0.0/0"]
+# Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "subnet-1"
+  region        = "us-central1"
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
 }
